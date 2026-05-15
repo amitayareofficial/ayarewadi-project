532304ef8258e628899e5d0939b91e9af262bcb3
amitayareofficial@gmail.com <amitayareofficial@gmail.com>
2026-05-16 01:39:01 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Ayarewadi Backend Running");
+});
+
+
 app.get("/events", async (req, res) => {
   const result = await pool.query("SELECT * FROM events");
   res.json(result.rows);
