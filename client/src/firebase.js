841c342a0e10c1d7685ab3a2df7da1852ca2aea7
amitayareofficial@gmail.com <amitayareofficial@gmail.com>
2026-05-24 19:21:22 +0530

    new change

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMF-xfxii8Zol-HQ5U5lvzDVGYiOUHWtI",
@@ -8,7 +7,7 @@ const firebaseConfig = {
   projectId: "ayarewadi-website",
   storageBucket: "ayarewadi-website.firebasestorage.app",
   messagingSenderId: "691909865303",
-  appId: "G-3SLWDYMDSY"
+  appId: "G-3SLWDYMDSY", // Firebase Console → Project Settings → Your apps → App ID (starts with 1:)
 };
 
 const app = initializeApp(firebaseConfig);
